5fff63da5df47b7b7f614b6a56be42d1952738cd
Ayush Sehgal <ayushs25@berkeley.edu>
2019-07-12 14:31:14 +0700

    Div 1 - Down Payment

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function getCarPriceTotal() {
     var form = document.getElementById("carform"); 
     var outputPrice = form.elements["totalPrice"];
@@ -6,5 +7,31 @@ function getCarPriceTotal() {
     //var totalPrice = getOriginalPrice() + getAddOnPrice();
 
     outputPrice.value = originalPrice + addOn;
-  
+    return outputPrice.value;
+}
+
+function getDownPayment() {
+    var downPayment = 0;
+    var form = document.getElementById["carform"];
+    var downPaymentList = form.elements["downPayment"];
+
+    for (let i = 0; i < downPaymentList.length; i++) {
+        if (downPaymentList[i].checked) {
+            downPayment = downPaymentList[i].value;
+            break;
+        }
+    }
+    return downPayment / 100;
+}
+
+function calcFinVal() {
+    var form = document.getElementById["carform"];
+    var carPrice = getCarPriceTotal();
+    var down = getDownPayment();
+    var finVal = carPrice * down;
+
+    var outputFinVal = form.elements["finVal"];
+    outputFinVal.value = finVal;
+    
+    return finVal;
 }
