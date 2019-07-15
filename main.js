f5fa5c126221bcfd325a321a285b77b621d22348
Ayush Sehgal <ayushs25@berkeley.edu>
2019-07-15 16:42:44 +0700

    Div 2 - Installment Value Cards (duplicate cards and delete)

--- main.js
+++ main.js
@@ -40,5 +40,36 @@ function calcFinVal() {
     var finVal = total * (down / 100);
 
     outputFinVal.value = finVal.toFixed(2);
-    
+    return finVal; 
+}
+
+function calcInstallments() {
+    var theForm2 = document.getElementById("interestCards");
+    console.log(theForm2);
+    var outputInstallment = theForm2.elements["installment"];
+    var years = parseInt(theForm2.elements["years"].value);
+    var interest = parseInt(theForm2.elements["interest"].value);
+    var financialValue = calcFinVal();
+    console.log(years);
+    console.log(interest);
+    console.log(financialValue);
+
+    var installmentValue = ((financialValue * (interest / 100) * years) + financialValue) / (years * 12);
+    console.log(installmentValue);
+
+    outputInstallment.value = installmentValue;
+    return installmentValue;
+}
+
+function addInterest() {
+   var installmentCard = document.getElementById("cards");
+   var duplicate = installmentCard.cloneNode(true);
+   duplicate.id = "interestCard2";
+   duplicate.onclick = addInterest;
+   installmentCard.parentNode.appendChild(duplicate);
+}
+
+function deleteInterest() {
+    var card = document.getElementById("cards");
+    card.parentNode.removeChild(card);
 }
