37390ce79dc4058bb41de9bda1ceba486ac951cf
Ayush Sehgal <ayushs25@berkeley.edu>
2019-07-21 15:27:09 +0700

    Set up Database Connection mySQL

--- connections/connect.php
+++ connections/connect.php
@@ -1,16 +1,25 @@
 <?php
-    // //Connect to Database 
-    // $conn = mysqli_connect('localhost', 'ayush', 'nandinisehgal', 'car_finances');
+    
+    if(!empty($_POST)) {
+        //Connect to Database 
+        $conn = mysqli_connect('localhost', 'ayush', 'nandinisehgal', 'car_finances');
+            //change to root asap 
+        //check connection
+        if (!$conn) {
+            echo 'Connection error: ' . mysqli_connect_error();
+        }
 
-    // //check connection
-    // if (!$conn) {
-    //     echo 'Connection error: ' . mysqli_connect_error();
-    // }
+        $sql = "INSERT INTO car_finances (name, cost, price) VALUES ('{$conn->real_escape_string($_POST['newAddOnName'])}','{$conn->real_escape_string($_POST['newAddOnCost'])}', '{$conn->real_escape_string($_POST['newAddOnPrice'])}')";
+        $insert = $conn->query($sql);
 
-    if(isset($_POST['add'])) {
-        echo $_POST['name'];
-        echo $_POST['cost'];
-        echo $_POST['price'];
+        if($insert) {
+            echo "Success! Row ID: {$conn->insert_id}";
+        } else {
+            die("Error: {$conn->errono} : {$conn->error}");
+        }
+
+        $conn->close();
     }
 
+    
 ?>
